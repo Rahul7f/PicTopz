02bc5afd5107e61cdc6dcff4db980a6764f88d43
rahul <rahulsingh.83748@gmail.com>
2020-11-26 15:48:16 +0530

    add intent

--- app/src/main/java/com/example/pictopz/SignUp.java
+++ app/src/main/java/com/example/pictopz/SignUp.java
@@ -2,13 +2,24 @@ package com.example.pictopz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignUp extends AppCompatActivity {
+    ImageView  imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        imageView = findViewById(R.id.gotohome);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUp.this,MainActivity.class));
+            }
+        });
     }
 }
