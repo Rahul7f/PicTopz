3d63cd0eae11ce7bd1121c44a4062664fa2acf78
rahul <rahulsingh.83748@gmail.com>
2020-11-26 12:03:37 +0530

    SplashScreen

---
+++ app/src/main/java/com/example/pictopz/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.pictopz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+
+    }
+}
