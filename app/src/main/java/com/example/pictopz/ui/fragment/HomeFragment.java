2627524b34453a70019a33c556d9f8f0728a1e4e
aadrsh <abajpai2001@gmail.com>
2020-12-14 21:47:43 +0530

    dirty story code

--- app/src/main/java/com/example/pictopz/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/example/pictopz/ui/fragment/HomeFragment.java
@@ -1,7 +1,7 @@
 package com.example.pictopz.ui.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +13,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.bumptech.glide.Glide;
-import com.example.pictopz.Profile;
 import com.example.pictopz.R;
 import com.example.pictopz.adapters.PostsAdapter;
 import com.example.pictopz.adapters.StoryAdapter;
@@ -26,23 +24,17 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
-import java.util.List;
 
 import xute.storyview.StoryModel;
 
@@ -51,16 +43,15 @@ public class HomeFragment extends Fragment {
 
     ImageView imageView;
     SwipeRefreshLayout swipeRefreshLayout;
-    ArrayList<ApprovedPostObject> approvedPostObjects=new ArrayList<>();
+    ArrayList<ApprovedPostObject> approvedPostObjects = new ArrayList<>();
     PostsAdapter postAdapter;
-    RecyclerView recyclerView1,recyclerView;
+    RecyclerView recyclerView1, recyclerView;
     StoryAdapter storyAdapter;
-    FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-    HashMap<String,ArrayList> storyHashMap=new HashMap<>();
-    ArrayList<String> keyset=new ArrayList<>();
-
-    ArrayList<String> following=new ArrayList<>();
+    HashMap<String, ArrayList> storyHashMap = new HashMap<>();
+    ArrayList<String> keyset = new ArrayList<>();
+    ArrayList<String> following = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -82,26 +73,20 @@ public class HomeFragment extends Fragment {
         swipeRefreshLayout.post(new Runnable() {
             @Override
             public void run() {
-//
+
 //                loadRecycleViewData();
 
             }
         });
 
 
-        // story
-//        MyStory currentStory = new MyStory(
-//                "dummy-link",
-//                //simpleDateFormat.parse("20-10-2019 10:00:00"),
-//                );
-
         fetchFollowList();
-        recyclerView=root.findViewById(R.id.home_story_recycleview);
-        storyAdapter =new StoryAdapter(storyHashMap,keyset);
+        recyclerView = root.findViewById(R.id.home_story_recycleview);
+        storyAdapter = new StoryAdapter(storyHashMap, keyset);
         recyclerView.setAdapter(storyAdapter);
         // post
-        recyclerView1=root.findViewById(R.id.home_layout_post_recycleview);
-        postAdapter=new PostsAdapter(getContext(),approvedPostObjects);
+        recyclerView1 = root.findViewById(R.id.home_layout_post_recycleview);
+        postAdapter = new PostsAdapter(getContext(), approvedPostObjects);
         recyclerView1.setAdapter(postAdapter);
 
         //TODO have to add a scroll listener so that more than 10 items can be displayed
@@ -109,53 +94,47 @@ public class HomeFragment extends Fragment {
         loadRecycleViewData();
 
 
-
         return root;
     }
 
     private void loadRecycleViewData() {
         swipeRefreshLayout.setRefreshing(true);
-
-        //fetching all followings
-        FirebaseDatabase.getInstance().getReference("following").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+        fetchFollowList();
+        CountDownTimer timer=new CountDownTimer(2000,2000) {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
-                    following.clear();
-                    for(DataSnapshot snapshot1:snapshot.getChildren())
-                        following.add(snapshot1.getKey());
-                    following.add(user.getUid());
-                    fetchPosts();
-                }
-                swipeRefreshLayout.setRefreshing(false);
+            public void onTick(long l) {
+
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onFinish() {
                 swipeRefreshLayout.setRefreshing(false);
             }
-        });
+        };
 
+        timer.start();
 
 
     }
 
-    private void fetchFollowList(){
+    private void fetchFollowList() {
 
         storyHashMap.clear();
         keyset.clear();
 
-        FirebaseDatabase.getInstance().getReference("following/"+user.getUid()).limitToFirst(50).addListenerForSingleValueEvent(new ValueEventListener() {
+        FirebaseDatabase.getInstance().getReference("following/" + user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists())
-
-                    for(DataSnapshot users:snapshot.getChildren()){
-
-                    fetchStories(users.getKey());
+                if (snapshot.exists()) {
+                    following.clear();
+                    for (DataSnapshot users : snapshot.getChildren()) {
+                        following.add(users.getKey());
+                    }
+                    following.add(user.getUid());
+                    fetchStories();
+                    fetchPosts();
                 }
 
-
             }
 
             @Override
@@ -165,71 +144,72 @@ public class HomeFragment extends Fragment {
         });
     }
 
-    private void fetchStories(String otherUserUID){
-        FirebaseDatabase.getInstance().getReference("story/"+otherUserUID).addListenerForSingleValueEvent(new ValueEventListener() {
+    private void fetchStories() {
+
+        CollectionReference reference = FirebaseFirestore.getInstance().collection("story");
+        reference
+                .whereIn("uploaderUID", following)
+                .orderBy("uploadTime", Query.Direction.DESCENDING)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists())
-                    for (DataSnapshot stories:snapshot.getChildren()){
-                        StoryObject object=stories.getValue(StoryObject.class);
-                        Log.e("Story List",object.storyID);
-                        deleteStoryIfTimeout(object,otherUserUID);
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot snapshot : task.getResult()) {
+                        StoryObject object = snapshot.toObject(StoryObject.class);
+                        deleteStoryIfTimeout(object, object.uploaderUID);
                     }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+                }
             }
         });
 
-
     }
 
-    private void deleteStoryIfTimeout(StoryObject object,String otherUserUID){
+    private void deleteStoryIfTimeout(StoryObject object, String otherUserUID) {
         //Delete Story if timout
         //Add to arraylist if valid
 
-        GregorianCalendar calendar=new GregorianCalendar();
-        long currentTime=calendar.getTimeInMillis();
-        if(object.uploadTime+28800000<currentTime){
-            FirebaseDatabase.getInstance().getReference("/story/"+otherUserUID+"/"+object.storyID).removeValue();
-        }else {
-            StoryModel model=new StoryModel(object.imageURL,object.uploaderUID,String.valueOf(object.uploadTime));
+        GregorianCalendar calendar = new GregorianCalendar();
+        long currentTime = calendar.getTimeInMillis();
+        if (object.uploadTime + 28800000 < currentTime) {
+            FirebaseFirestore.getInstance().collection("story").document(object.storyID).delete();
+        } else {
+            StoryModel model = new StoryModel(object.imageURL, object.uploaderUID, String.valueOf(object.uploadTime));
 
-            if(storyHashMap.containsKey(object.uploaderUID)){
+            if (storyHashMap.containsKey(object.uploaderUID)) {
                 storyHashMap.get(object.uploaderUID).add(model);
-            }else{
-                ArrayList<StoryModel> models=new ArrayList<>();
+            } else {
+                ArrayList<StoryModel> models = new ArrayList<>();
                 models.add(model);
-                storyHashMap.put(object.uploaderUID,(ArrayList) models);
+                storyHashMap.put(object.uploaderUID, (ArrayList) models);
                 keyset.add(object.uploaderUID);
             }
         }
         storyAdapter.notifyDataSetChanged();
     }
 
-    private void fetchPosts(){
+    private void fetchPosts() {
         following.add("CONTEST");
-        CollectionReference dbRef=FirebaseFirestore.getInstance().collection("posts");
-        Query query=dbRef
+        CollectionReference dbRef = FirebaseFirestore.getInstance().collection("posts");
+        Query query = dbRef
                 .whereEqualTo("approved", true)
-                .whereIn("filterID",following)
+                .whereIn("filterID", following)
                 .orderBy("timestamp", Query.Direction.DESCENDING);
 
         query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     approvedPostObjects.clear();
-                    for(QueryDocumentSnapshot snapshot1:task.getResult()) {
+                    for (QueryDocumentSnapshot snapshot1 : task.getResult()) {
                         approvedPostObjects.add(snapshot1.toObject(ApprovedPostObject.class));
-                        Log.e("POST","LIKES "+approvedPostObjects.get(0).commentsNo);
+                        Log.e("POST", "LIKES " + approvedPostObjects.get(0).commentsNo);
                     }
                     postAdapter.notifyDataSetChanged();
                 }
             }
         });
     }
+
 
 }
