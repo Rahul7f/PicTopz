44615901220e6f4fbf1bd2ae81a2ff7eee861234
rahul <rahulsingh.83748@gmail.com>
2020-12-14 19:06:22 +0530

    user can uplaod photo

--- app/src/main/java/com/example/pictopz/StoryPhotoUpload.java
+++ app/src/main/java/com/example/pictopz/StoryPhotoUpload.java
@@ -1,13 +1,18 @@
 package com.example.pictopz;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,9 +28,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.IOException;
 import java.util.GregorianCalendar;
 import java.util.UUID;
+
+import static android.app.Activity.RESULT_OK;
 
 public class StoryPhotoUpload extends Fragment {
 
@@ -72,6 +81,7 @@ public class StoryPhotoUpload extends Fragment {
     {
         if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
         {
+            Toast.makeText(getContext(), "if run", Toast.LENGTH_SHORT).show();
             try {
                 requestPermissions(new  String[] {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.CAMERA},100);
 
@@ -81,8 +91,49 @@ public class StoryPhotoUpload extends Fragment {
             }
         }
         else {
+            Toast.makeText(getContext(), "else run", Toast.LENGTH_SHORT).show();
             pickimage();
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==100 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+        {
+            pickimage();
+        }
+        else
+        {
+            Toast.makeText(getContext(), "This Permission is required", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri imageUri = CropImage.getPickImageResultUri(getContext(), data);
+            croprequest(imageUri);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode==RESULT_OK)
+            {
+                try {
+                    filePath=result.getUri();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),result.getUri());
+                    Toast.makeText(getContext(), "image uri:- "+bitmap, Toast.LENGTH_SHORT).show();
+                    imageView.setImageBitmap(bitmap);
+                }catch (IOException e)
+                {
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), "error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void pickimage()
@@ -121,6 +172,16 @@ public class StoryPhotoUpload extends Fragment {
             }
         });
 
+    }
+
+    public void croprequest(Uri imageURL)
+    {
+        Fragment fragment=getParentFragmentManager().findFragmentByTag("UPLOAD");
+
+        CropImage.activity(imageURL)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setMultiTouchEnabled(true)
+                .start(getContext(),fragment);
     }
 
 }
