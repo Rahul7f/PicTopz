e41e7130caa47e16681b54beed966ca2f5de39de
aadrsh <abajpai2001@gmail.com>
2020-12-14 16:09:45 +0530

    bindu

--- app/src/main/java/com/example/pictopz/adapters/ProfileGridAdapter.java
+++ app/src/main/java/com/example/pictopz/adapters/ProfileGridAdapter.java
@@ -15,11 +15,7 @@ import com.example.pictopz.R;
 import com.example.pictopz.models.UnApprovedDataObject;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
