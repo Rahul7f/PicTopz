c06c763b0bf481104351c0666d98e593270a65d3
aadrsh <abajpai2001@gmail.com>
2020-12-15 11:29:56 +0530

    TimeFIx in ShowOneContest

--- app/src/main/java/com/example/pictopz/ui/fragment/ShowOneContest.java
+++ app/src/main/java/com/example/pictopz/ui/fragment/ShowOneContest.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.pictopz.R;
+import com.example.pictopz.firebase.MyCountDownTimer;
 import com.example.pictopz.models.ContestObject;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -35,9 +36,6 @@ import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class ShowOneContest extends Fragment {
-
-    GregorianCalendar calendar= new GregorianCalendar();
-    String oldTime = calendar.get(GregorianCalendar.DAY_OF_MONTH)+"."+calendar.get(GregorianCalendar.MONTH)+"."+calendar.get(GregorianCalendar.YEAR)+", "+calendar.get(GregorianCalendar.HOUR)+":"+calendar.get(GregorianCalendar.MINUTE)+":"+calendar.get(Calendar.SECOND);
 
     FirebaseAuth mAuth;
     ContestObject contestObject;
@@ -75,12 +73,7 @@ public class ShowOneContest extends Fragment {
 
 
 
-        MyCount myCount=new MyCount(convertDate(contestObject.timeStart),1000) {
-            @Override
-            public void changeTime(String hms) {
-                textView.setText(hms);
-            }
-        };
+        MyCountDownTimer myCount=new MyCountDownTimer(contestObject.timeStart,textView) ;
         myCount.start();
 
         upload.setOnClickListener(new View.OnClickListener() {
@@ -100,45 +93,6 @@ public class ShowOneContest extends Fragment {
         return root;
         }
 
-    public abstract class MyCount extends CountDownTimer {
-        MyCount(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-        }
-
-        @Override
-        public void onFinish() {
-
-        }
-
-        @Override
-        public void onTick(long millisUntilFinished) {
-            long millis = millisUntilFinished;
-            String hms = (TimeUnit.MILLISECONDS.toDays(millis)) + ":"
-                    + (TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis)) + ":")
-                    + (TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)) + ":"
-                    + (TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))));
-            changeTime(hms);
-        }
-        public abstract void changeTime(String hms);
-    }
-
-    public long convertDate(Long NewLong){
-        Long diff=0l;
-        Long oldLong=0l;
-
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy, HH:mm:ss");
-
-        Date oldDate;
-
-        try {
-            oldDate = formatter.parse(oldTime);
-            oldLong = oldDate.getTime();
-            diff = NewLong - oldLong;
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return diff;
-    }
 
     private void fetchLimit(){
         upload.setEnabled(false);
