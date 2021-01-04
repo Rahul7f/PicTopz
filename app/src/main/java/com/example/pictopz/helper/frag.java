8cb9a780b0c084244d0464d89b57bfeebf97b0a5
aadrsh <abajpai2001@gmail.com>
2021-01-04 20:21:21 +0530

    added missing class

---
+++ app/src/main/java/com/example/pictopz/helper/frag.java
@@ -0,0 +1,9 @@
+package com.example.pictopz.helper;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.HashMap;
+
+public class frag {
+    public static HashMap<String, Fragment> hashMap = new HashMap<>();
+}
